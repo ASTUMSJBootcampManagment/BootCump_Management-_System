c11afd91656ea96a4b8ae15465208d826424a05c
Hafsa <hafsaabdulwahid565@gmail.com>
2026-08-15 10:56:03 +0300

    added things

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,16 @@
 const dotenv = require("dotenv").config();
 require("dotenv").config();
 const dns = require("dns");
-
-// Set DNS servers globally for the entire Node.js process
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const authRoutes = require("./routes/authroutes");
 const userRoutes = require("./routes/userRoutes");
-
 dbConnect();
-
 const app = express();
-
 app.use(express.json());
-
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-
-// Start the server
 const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,15 @@
 const express=require("express")
 const router= express.Router();
+const verifyToken = require("../middlewares/authMiddleware");
+const authorizeRoles = require("../middlewares/roleMiddleware");
 
-router.get("/admin",(req,res)=>{
+router.get("/admin",verifyToken, authorizeRoles("Admin"),(req,res)=>{
     res.json({message:"Welcome Admin"})
 });
-router.get("/mentor",(req,res)=>{
+router.get("/mentor",verifyToken, authorizeRoles("Admin","Mentor"),(req,res)=>{
     res.json({message:"Welcome Mentor"})
 });
-router.get("/student",(req,res)=>{
+router.get("/student",verifyToken, authorizeRoles("Admin","Mentor","Student"),(req,res)=>{
     res.json({message:"Welcome student"})
 });
 module.exports=router;
