b48931bfde10f38ee04db15381edbd297e7d21d0
eman719 <emanabdullaziz719@gmail.com>
2026-08-22 17:40:44 +0300

    side and nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage"
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import AdminDashboard from "./pages/AdminDashbord";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/admin" element={<AdminDashboard />} />
+
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
