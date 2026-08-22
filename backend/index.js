4ba7528693f7b33abfd7c5887ef5d9f350b7e9a8
Asanti Nuredin <asantinuredin739@gmail.com>
2026-08-22 15:58:59 +0300

    Add assignments, submissions, and grading system

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,13 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect");
 const authRoutes = require("./routes/authroutes");
 const userRoutes = require("./routes/userRoutes");
+const assignmentRoutes = require("./routes/assignmentRoutes");
 dbConnect();
 const app = express();
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/assignments", assignmentRoutes);
 const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
